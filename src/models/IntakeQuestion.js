bc9174b521d2c2736432da5e7c78d486aa24356d
harmeetkaursohi <harmeetkaursohi95@gmail.com>
2025-11-19 12:16:01 +0530

    updates

--- src/models/IntakeQuestion.js
+++ src/models/IntakeQuestion.js
@@ -1,39 +1,45 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const optionSchema = new mongoose.Schema({
-  text: {
-    type: String,
-    required: true
+const optionSchema = new mongoose.Schema(
+  {
+    text: {
+      type: String,
+      required: true,
+    },
+    comment: Array,
+    actionTypeA: String,
+    actionTypeB: String,
   },
-  comment: String,
-  actionTypeA: String,
-  actionTypeB: String
-}, { _id: false });
+  { _id: false }
+);
 
-const intakeQuestionSchema = new mongoose.Schema({
-  questionId: {
-    type: String,
-    required: true,
-    unique: true
+const intakeQuestionSchema = new mongoose.Schema(
+  {
+    questionId: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    questionText: {
+      type: String,
+      required: true,
+    },
+    inputType: {
+      type: String,
+      enum: ["free_text", "single_select", "multi_select"],
+      required: true,
+    },
+    defaultComment: String,
+    options: [optionSchema],
+    position: {
+      type: Number,
+      required: true,
+    },
   },
-  questionText: {
-    type: String,
-    required: true
-  },
-  inputType: {
-    type: String,
-    enum: ['free_text', 'single_select', 'multi_select'],
-    required: true
-  },
-  defaultComment: String,
-  options: [optionSchema],
-  position: {
-    type: Number,
-    required: true
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 // Create index for ordering
 intakeQuestionSchema.index({ position: 1 });
 
-module.exports = mongoose.model('IntakeQuestion', intakeQuestionSchema);
+module.exports = mongoose.model("IntakeQuestion", intakeQuestionSchema);
